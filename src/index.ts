50deec124078365bab2c42705adcb9d6a6694828
keleris32 <charles.ejiegbu@gmail.com>
2022-03-09 01:27:28 +0100

    feat: added swagger-ui-express package for api documentation

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ require('dotenv').config();
 // Packages
 import express, { Application } from 'express';
 import cors from 'cors';
+import { serve, setup } from 'swagger-ui-express';
 import todoRouter from './routes/todosRoutes';
+const docs = require('./docs');
 
 const app: Application = express();
 
@@ -12,6 +14,7 @@ app.use(express.json()); // Parse incoming data to JSON
 
 // API routes
 app.use('/api', todoRouter); // Todos route
+app.use('/api-docs', serve, setup(docs));
 
 app.listen(process.env.PORT || 4000, (): void =>
   console.log('Connected ah successfully!')
